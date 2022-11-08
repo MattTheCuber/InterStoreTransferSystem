87e2fff0366680077f085a507e73619664db9651
MattTheCuber <mattvine03@gmail.com>
2022-11-08 11:30:20 -0500

    remove unused line

--- back-end/index.js
+++ back-end/index.js
@@ -18,5 +18,3 @@ app.get("/v1/items/:itemId", StoreItemsRoute);
 app.get("/v1/requests", RequestsRoute);
 app.post("/v1/requests/create", CreateRequestRoute);
 app.listen(3030, () => console.log("[API] Now listening on port 3030"));
-
-// http://localhost:3030/v1/requests/incoming?storeId=91763&key=bc547750b92797f955b36112cc9bdd5cddf7d0862151d03a167ada8995aa24a9ad24610b36a68bc02da24141ee51670aea13ed6469099a4453f335cb239db5da
